a5ee05f08dabe40080edd155a476488f776ab1f8
Svitlana <lyulkalyulka@gmail.com>
2021-12-25 23:28:51 +0200

    Add pull project

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Chat from './components/homework/bsa';
+import Chat from './components/chat/chat';
 
 
 ReactDOM.render(
